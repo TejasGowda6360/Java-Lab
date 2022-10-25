d5bb8ff10166c1df52ce19fa8c5657759304abdb
TejasGowda6360 <116062491+TejasGowda6360@users.noreply.github.com>
2022-10-25 13:20:48 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,17 @@
+package myjava;
+
+public class assignment {
+public static void main(String[] args) {
+	int num1 =10;
+	num1 +=5;
+	System.out.println("The Output of num1 += 5 is" +num1);
+	num1 -=5;
+	System.out.println("The Output of num1 -= 5 is" +num1);
+	num1 *=5;
+	System.out.println("The Output of num1 *= 5 is" +num1);
+	num1 /=5;
+	System.out.println("The Output of num1 /= 5 is" +num1);
+	num1 %=5;
+	System.out.println("The Output of num1 %= 5 is" +num1);
+}
+}
---
+++ turnery.java
@@ -0,0 +1,11 @@
+package myjava;
+
+public class turnery {
+	public static void main(String[] args) {
+	int a=6,b=10;
+	String result = (a==5)?"Equal":"Not Equal";
+	System.out.println(result);
+	result = (b==10)?"Equal":"Not Equal";
+	System.out.println(result);
+	}
+}
